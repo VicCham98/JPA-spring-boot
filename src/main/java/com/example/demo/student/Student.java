ef64493ec44474a096170443f80a691b37751408
VicCham98 <41102257+VicCham98@users.noreply.github.com>
2022-10-31 18:24:27 -0500

    init config

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,106 @@
+package com.example.demo.student;
+
+import org.springframework.util.StringUtils;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private LocalDate doB;
+    private String email;
+    private String address;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, Integer age, LocalDate doB, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.doB = doB;
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return  address == null ? "vacio" : address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Student(String name, Integer age, LocalDate doB, String email) {
+        this.name = name;
+        this.age = age;
+        this.doB = doB;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDoB() {
+        return doB;
+    }
+
+    public void setDoB(LocalDate doB) {
+        this.doB = doB;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", doB=" + doB +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
